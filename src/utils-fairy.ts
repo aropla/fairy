a628bafa4838b85d9bb4b20489ca5f67b5ef1a26
ayarin <me@ayarin.moe>
2024-02-20 15:57:13 +0800

    feat: common utils

---
+++ src/utils-fairy.ts
@@ -0,0 +1,7 @@
+export const Noop = () => {}
+export function isFunction(value: unknown) {
+  return typeof value === 'function'
+}
+export function isNumber(value: unknown) {
+  return typeof value === 'number'
+}
